7d2103b30e635f4effda816595b77a55cf14abb8
DEVU <marcineryk@outlook.com>
2026-02-06 13:04:15 +0100

    Fixed batery/power_suply driver.

--- install-alioth.sh
+++ install-alioth.sh
@@ -1,3 +1,3 @@
 sudo cp -f arch/arm64/boot/Image.gz /mnt/alioth_boot/vmlinuz_linux
 sudo cp -f arch/arm64/boot/dts/qcom/sm8250-xiaomi-alioth.dtb /mnt/alioth_boot/sm8250-xiaomi-alioth.dtb
-sudo make ARCH=arm64 INSTALL_MOD_PATH=~/alioth_root modules_install
+sudo make ARCH=arm64 INSTALL_MOD_PATH=/mnt/alioth_root modules_install
